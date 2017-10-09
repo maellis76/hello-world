7e529d020b04c7e35073f977c413b408859dfb61
Michael E <maellis76@users.noreply.github.com>
2017-10-09 15:57:40 -0600

    Playing with Code
    Added to the script

---
+++ script.js
@@ -0,0 +1,11 @@
+//console.log("Hello world!");
+
+var isMarried = false;
+var name = "John";
+var age = 47;
+
+if (isMarried) {
+    console.log("Hello world! " + name + ", " + age + ", is married.");
+} else {
+    console.log("Hello world! " + name + ", " + age + ", is not married.");
+}
